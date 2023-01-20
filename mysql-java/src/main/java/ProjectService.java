b74b1e12060a1e5956682ef5705a8b2aa84d4615
BraxtonDHill <116772387+BraxtonDHill@users.noreply.github.com>
2023-01-20 14:52:46 -0600

    Add files via upload

---
+++ mysql-java/src/main/java/ProjectService.java
@@ -0,0 +1,9 @@
+import projects.entity.Project;
+
+public class ProjectService {
+	private ProjectDao projectDao = new ProjectDao();
+	
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+}
